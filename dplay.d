3348ff9ba4f0e02dd3bc4b7b5d120432185b5cf0
MggMuggins <mggmugginsmc@gmail.com>
2017-02-07 14:24:38 -0600

    More or less working source, and install script(Not functional on most systems)

--- dplay.d
+++ dplay.d
@@ -4,7 +4,7 @@ import std.stdio : write;
 import std.stdio;
 import std.file;
 import std.conv : to;
-import std.algorithm : remove;
+//import std.algorithm : remove;
 
 class Letter {
     char[][] letter;
@@ -37,28 +37,66 @@ char[] removeNewlines(char[] stuff) {
 }
 
 int main(string[] args) {
-    int i = 0;
-    auto aL = new Letter();
+    int j = 0, i = 0;
+    File lib;
+    Letter[char] letters;
+    string basePath = "/home/wesley/workspace/DPlay/fonts/";
+    string font = "default/";
+    char[] path;
+    path ~= basePath ~= font ~= ' ';
+    char[] input = to!(char[])(args[1]);
+    //path[path.length - 1] = 'T';
+    writeln("Setup done in: ");
+    writeln(path);
+
+    for(i = 0; i < input.length; ++i) {
+        letters[input[i]] = new Letter();
+        path[path.length - 1] = input[i];
+        lib.open(to!string(path), "r");
+        while (!lib.eof) {
+            letters[input[i]].addLine(to!(char[])(lib.readln()));
+        }
+        lib.close();
+    }
+    writeln("Read into Objects done.");
+
+    //writeln(input);
+
+    //*******************************************
+    /*Not needed right now
+    letters['a'] = new Letter();
     File aF = File("/home/wesley/workspace/DPlay/fonts/default/a", "r");
-    auto GL = new Letter();
-    File GF = File("/home/wesley/workspace/DPlay/fonts/default/G", "r");
+    letters['g'] = new Letter();
+    File gF = File("/home/wesley/workspace/DPlay/fonts/default/g", "r");
 
     writeln("Done Creating Objects");
 
     while (!aF.eof) {
-        aL.addLine(to!(char[])(aF.readln()));
+        letters['a'].addLine(to!(char[])(aF.readln()));
     }
-    while (!GF.eof) {
-        GL.addLine(to!(char[])(GF.readln()));
+    while (!gF.eof) {
+        letters['g'].addLine(to!(char[])(gF.readln()));
     }
 
     writeln("Done Reading to objects.");
-
-    for(i = 0; i < aL.letter.length; ++i) {
-        aL.writeLine(i);
-        GL.writeLine(i);
+    *
+    for(i = 0; i < 7; ++i) {
+        letters['T'].writeLine(i);
+        letters['e'].writeLine(i);
+        letters['s'].writeLine(i);
+        letters['t'].writeLine(i);
         writeln();
     }
-    //aL.writeSelf();
+    //aL.writeSelf();*/
+
+    writeln("Beginning write-out");
+    writeln("While condition = ", letters[input[0]].letter.length);
+    do {
+        for(i = 0; i < input.length; ++i) {
+            letters[input[i]].writeLine(j);
+        }
+        writeln();
+        ++j;
+    } while(j < letters[input[0]].letter.length);
 	return 0;
 }
